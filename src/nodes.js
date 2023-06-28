46afb6d7a9cd88cd9eb144cfa0bb0a78b059ba0a
badr <bernardoantoniod@gmail.com>
2023-06-27 21:29:53 -0400

    archivo nodes para utilizar en las funciones de navegación

---
+++ src/nodes.js
@@ -0,0 +1,52 @@
+// en este archivo se van a traer todos los nodos para activar/desactivar según la pantalla que estemos viendo.
+//los comentados son los del profesor.
+//luego puse los mios con sus mismos nombres y solo algunas veces cambian los selectores.
+
+
+//const headerSection=document.querySelector('#header');
+const headerSection = document.querySelector("#header");
+
+//const trendingPreviewSection=document.querySelector('#trendingPreview');
+const trendingPreviewSection = document.querySelector("#trendingPreview");
+
+//const categoriesPreviewSection=document.querySelector('#categoriesPreview');
+const categoriesPreviewSection = document.querySelector("#categories-container");
+
+//const genericSection=document.querySelector('#genericList');
+const genericSection = document.querySelector("#genericList");
+
+//const movieDetailSection=document.querySelector('#movieDetail');
+const movieDetailSection = document.querySelector("#movieDetail");
+
+//const searchForm=document.querySelector('#searchForm');
+const searchForm = document.querySelector("#search-form"); 
+
+//const trendingMoviesPreviewList=document.querySelector('.trendingPreview-movieList');
+const trendingMoviePreviewList = document.querySelector(".trending-movie-poster-description-container");
+
+//const trendingBtn=document.querySelector('.trendingPreview-btn');
+const trendingBtn = document.querySelector(".trending-button");
+
+//const categoriesPreviewList=document.querySelector('.categoriesPreview-list');
+const categoriesPreviewList = documet.querySelector(".categories-type-container");
+
+//const headerTitle=document.querySelector('.header-title');
+const headerTitle = document.querySelector(".header-title");
+
+//const headerCategoryTitle=document.querySelector('.header-title--categoryView');
+const headerCategoryTitle = document.querySelector(".header-title categoryView");
+
+//const arrowBtn=document.querySelector('.header-arrow');
+const arrowBtn = document.querySelector(".header-arrow");
+
+//const searchFormInput=document.querySelector('#searchForm input');
+const searchFormInput = document.querySelector("#search-form input")
+
+//const searchFormBtn=document.querySelector('#searchBtn');
+const searchFormBtn = document.querySelector("#searchBtn")
+
+const movieDetailTitle=document.querySelector('.movieDetail-title');
+const movieDetailScore=document.querySelector('.movieDetail-score');
+const movieDetailDescription=document.querySelector('.movieDetail-description');
+const movieDetailCategoriesList=document.querySelector('#movieDetail .categories-list');
+const relatedMoviesContainer=document.querySelector('.relatedMovies-scrollContainer');
